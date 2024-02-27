ac1440ce5f5058a47705a6392f3fc9f0ae2516bd
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-02-27 18:34:15 +0700

    feat: add model

--- src/MusicGenreDataset.py
+++ src/MusicGenreDataset.py
@@ -6,7 +6,6 @@ import pandas as pd
 from datetime import datetime
 from torch.utils.data import Dataset
 
-
 AUDIO_FEATURES = [
     "danceability",
     "energy",
@@ -22,222 +21,113 @@ AUDIO_FEATURES = [
 ]
 
 
-class InputType:
-    UNCLEAN = "unclean"
-    CLEAN = "clean"
-    INDEX = "index"
-
-
 class MusicGenreDataset(Dataset):
-    def __init__(
-        self,
-        path_data: str,
-        max_seq_length: int = 250,
-    ):
+    def __init__(self, path_data, max_seq_length=250):
         self.max_seq_length = max_seq_length
-        self.X, self.X_audio, self.y_label, self.y_id = self._load_data(path_data)
+        self.X, self.y_id, self.y_label = self._load_data(path_data)
 
-    def __len__(self) -> int:
+    def __len__(self):
         return len(self.X)
 
-    def __iter__(self) -> any:
-        for index in range(len(self)):
-            yield self[index]
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
 
-    def __getitem__(self, index) -> tuple:
-        return (
-            torch.tensor(self.X[index], dtype=torch.int64),
-            torch.tensor(self.X_audio[index], dtype=torch.float32),
-            torch.tensor(self.y_id[index], dtype=torch.int64),
+    def __getitem__(self, index):
+        _X = np.zeros(self.max_seq_length, dtype=np.int64)
+        item = self.X[index]
+        max_index = (
+            self.max_seq_length if self.max_seq_length < len(item) else len(item)
         )
+        _X[:max_index] = np.int64(item[:max_index]) + 1
+        return (index, _X, self.y_id[index], self.y_label[index])
 
-    def _load_data(self, path_data: str) -> pd.DataFrame:
+    def _load_data(self, path_data):
         data = pd.read_csv(path_data, index_col=0)
-        X, X_audio, y_label, y_id = self._load_raw_data(data)
-        return X, X_audio, y_label, y_id
+        X_lyrics = data["lyrics"].values
+        X_audio = data[AUDIO_FEATURES].values
+        X = np.concatenate([X_lyrics.reshape(-1, 1), X_audio], axis=1)
 
-    def _load_raw_data(self, data: pd.DataFrame) -> tuple:
-        X = data["lyrics"].values
-        return (
-            X,
-            data[AUDIO_FEATURES],
-            data["playlist_genre"],
-            data["playlist_genre_id"],
-        )
+        y_id = data["playlist_genre_id"].values
+        y_label = data["playlist_genre"].values
+
+        return X, y_id, y_label
+
+
+def batch_to_tensor(batch):
+    index, _X, target_id, target_label = zip(*batch)
+    return torch.LongTensor(_X), torch.LongTensor(target_id)
 
 
 class MusicGenreDatasetWithPreprocess(Dataset):
     def __init__(
         self,
-        path_data: str,
-        embedder_model: any,
-        embedder_type: str,
-        input_type: InputType,
-        max_seq_length: int = 250,
-        store_processed: bool = True,
-        output_dir: str = "",
+        path_data,
+        embedding_model,
+        embedding_type,
+        max_seq_length=250,
+        input_type="index",
+        store_preprocess=False,
+        output_dir="",
     ):
         self.input_type = input_type
-        self.embedder_model = embedder_model
-        self.embedder_type = embedder_type
+        self.emb_model = embedding_model
+        self.emb_type = embedding_type
+        self.store_preprocess = store_preprocess
         self.max_seq_length = max_seq_length
-        self.store_processed = store_processed
-        self.output_dir = os.path.abspath(output_dir)
-        self.nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
+        self.output_dir = output_dir
 
-        if self.embedder_type == "bert":
-            self.func_w2v = self._bert_w2v
-        elif self.embedder_type == "gensim":
-            self.func_w2v = self._gensim_w2v
+        if self.input_type == "unclean":
+            self.nlp = spacy.load("en_core_web_sm", disable=["tagger", "parser", "ner"])
+
+        if self.emb_type == "bert":
+            self.func_w2i = self._bert_w2id
+        elif self.emb_type == "gensim":
+            self.func_w2i = self._gensim_w2id
         else:
-            raise ValueError(f"Unsupported embedder type: {self.embedder_type}")
+            os.error(f"Embedding type {self.emb_type} not supported")
 
-        self.X, self.X_audio, self.y_label, self.y_id = self._load_data(path_data)
+        self.X, self.y_id, self.y_label = self._load_data(path_data)
 
-    def __len__(self) -> int:
+    def __len__(self):
         return len(self.X)
 
-    def __iter__(self) -> any:
-        for index in range(len(self)):
-            yield self[index]
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
 
-    def __getitem__(self, index) -> tuple:
-        item_text = self.X[index]
+    def __getitem__(self, index):
+        _X = np.zeros(self.max_seq_len, dtype=np.int64)
+        item = self.X[index]
 
-        if self.embedder_type == "bert":
-            input_text = self.embedder_model.encode_plus(
-                item_text,
-                add_special_tokens=True,
-                max_length=self.max_seq_length,
-                pad_to_max_length=True,
-                return_attention_mask=True,
-                return_token_type_ids=True,
-            )
-            ids_text = input_text["input_ids"]
-            mask_text = input_text["attention_mask"]
-            token_type_ids_text = input_text["token_type_ids"]
-        elif self.embedder_type == "gensim":
-            ids_text = [int(word) for word in item_text.split()]
-            mask_text = np.ones(self.max_seq_length, dtype=np.int64)
-            token_type_ids_text = np.zeros(self.max_seq_length, dtype=np.int64)
-        else:
-            raise ValueError(f"Unsupported embedder type: {self.embedder_type}")
+        if not self.store_preprocess:
+            if self.input_type == "unclean":
+                item = self.preprocess(self.nlp, " ".join(item))
+                item = self.func_w2i(item)
+            elif self.input_type == "clean":
+                item = self.func_w2i(item)
+            elif self.input_type == "index":
+                pass
+            else:
+                os.error(f"Input type {self.input_type} not supported")
 
-        return (
-            torch.tensor(ids_text, dtype=torch.int64),
-            torch.tensor(mask_text, dtype=torch.int64),
-            torch.tensor(token_type_ids_text, dtype=torch.int64),
-            torch.tensor(self.X_audio[index], dtype=torch.float32),
-            torch.tensor(self.y_id[index], dtype=torch.int64),
+        max_index = (
+            self.max_seq_length if self.max_seq_length < len(item) else len(item)
         )
+        const = 1 if self.emb_type == "gensim" else 0
+        _X[:max_index] = np.int64(item[:max_index]) + const
+        return (index, _X, self.y_id[index], self.y_label[index])
 
-    def _process_item(self, item) -> list:
-        if self.input_type == InputType.UNCLEAN:
-            item = self.preprocess(self.nlp, " ".join(item))
-            item = self.func_w2v(item)
-        elif self.input_type == InputType.CLEAN:
-            item = self.func_w2v(item)
-        elif self.input_type == InputType.INDEX:
-            pass
-        else:
-            raise ValueError(f"Unsupported input type: {self.input_type}")
-
-        return item
-
-    def _load_data(self, path_data: str) -> pd.DataFrame:
+    def _load_data(self, path_data):
         data = pd.read_csv(path_data, index_col=0)
+        if self.store_preprocess and self.input_type != "index":
+            print(f"Storing preprocessed data to {self.output_dir}")
 
-        if not os.path.exists(self.output_dir):
-            os.makedirs(self.output_dir)
-
-        if self.store_processed and self.input_type != InputType.INDEX:
-            X, X_audio, y_label, y_id = self._process_data(data)
-            path_processed = os.path.join(
-                self.output_dir,
-                f"ids_{datetime.now().strftime('%y-%m-%d_%H%M%S')}.csv.zip",
-            )
-
-            tmp = pd.DataFrame(
-                {
-                    "lyrics": [" ".join(map(str, lyrics)) for lyrics in X.values],
-                    "_lyrics": data["lyrics"].values,
-                    **{feature: data[feature].values for feature in AUDIO_FEATURES},
-                    "playlist_genre": data["playlist_genre"].values,
-                    "playlist_genre_id": data["playlist_genre_id"].values,
-                }
-            )
-            tmp.to_csv(path_processed, compression="zip")
-        else:
-            X, X_audio, y_label, y_id = self._load_raw_data(data)
-
-        return X, X_audio, y_label, y_id
-
-    def _process_data(self, data: pd.DataFrame) -> tuple:
-        if self.input_type == InputType.UNCLEAN:
-            X = [self.preprocess(self.nlp, lyrics) for lyrics in data["lyrics"].values]
-            X = [self.func_w2v(tokens) for tokens in X]
-            X = pd.DataFrame(X)
-        elif self.input_type == InputType.CLEAN:
-            X = [lyrics.split() for lyrics in data["lyrics"].values]
-            X = [self.func_w2v(tokens) for tokens in X]
-            X = pd.DataFrame(X)
-        elif self.input_type == InputType.INDEX:
-            X = [
-                [int(word) for word in lyrics.split()]
-                for lyrics in data["lyrics"].values
-            ]
-        else:
-            raise ValueError(f"Unsupported input type: {self.input_type}")
-
-        return (
-            X,
-            data[AUDIO_FEATURES],
-            data["playlist_genre"],
-            data["playlist_genre_id"],
-        )
-
-    def _load_raw_data(self, data: pd.DataFrame) -> tuple:
-        if self.input_type == InputType.INDEX:
-            X = [
-                [int(word) for word in lyrics.split()]
-                for lyrics in data["lyrics"].values
-            ]
-        else:
-            X = [[word for word in lyrics.split()] for lyrics in data["lyrics"].values]
-
-        return (
-            X,
-            data[AUDIO_FEATURES],
-            data["playlist_genre"],
-            data["playlist_genre_id"],
-        )
-
-    def _bert_w2v(self, tokens: list) -> list:
-        return [self.embedder_model.encode(token) for token in tokens]
-
-    def _gensim_w2v(self, tokens: list) -> list:
-        return [
-            self.embedder_model[token]
-            for token in tokens
-            if token in self.embedder_model
-        ]
-
-    @staticmethod
-    def preprocess(nlp, text: str) -> list:
-        """
-        Perform text preprocessing using spaCy.
-
-        Parameters:
-        - nlp (spacy.lang.en.English): SpaCy language model.
-        - text (str): Input text.
-
-        Returns:
-        - list: Processed list of tokens.
-        """
-        doc = nlp(text)
-        out = [
-            token.lemma_.lower()
-            for token in doc
-            if not token.is_stop and token.lemma_.isalpha()
-        ]
-        return out
+            if self.input_type == "unclean":
+                X_lyrics = [
+                    self.preprocess(self.nlp, " ".join(item))
+                    for item in data["lyrics"].values
+                ]
+                X_lyrics = [self.func_w2i(item) for item in X_lyrics]
+            elif self.input_type == "clean":
+                pass
--- src/model/ModelBERT.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-import numpy as np
-from transformers import AutoModel
-
-
-class ModelBERT(nn.Module):
-    def __init__(self):
-        super(ModelBERT, self).__init__()
